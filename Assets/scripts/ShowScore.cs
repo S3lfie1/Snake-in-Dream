e64069a3ae59917febd5f65aa4921e780093f8f1
Jakub Mazur <32875359+S3lfie1@users.noreply.github.com>
2018-03-04 20:54:22 +0100

    Add files via upload

---
+++ Assets/scripts/ShowScore.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowScore : MonoBehaviour {
+	public static int score = 0;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "Score: " + Score.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score = FruitRotator.count;
+		scoreText.text = "Score: " + score;
+	}
+}
